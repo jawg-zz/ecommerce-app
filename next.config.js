0c08f30f2a236d673b48b01413d0cb126db1e38d
jawg-zz <mosetigeorge@gmail.com>
2026-03-31 07:45:16 +0000

    fix: re-enable image optimization with remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+    ],
   },
   experimental: {
     instrumentationHook: true,
